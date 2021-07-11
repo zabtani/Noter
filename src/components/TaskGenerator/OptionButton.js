8ed7f5a653bf064188d659a3211ae5765fafef22
zabtani <zabtani@gmail.com>
2021-07-11 13:20:41 +0300

    update label delete button design

--- src/components/TaskGenerator/OptionButton.js
+++ src/components/TaskGenerator/OptionButton.js
@@ -1,9 +1,20 @@
 import classes from './TaskGenerator.module.css';
-
+import LabelDeleteIcon from './LabelDeleteIcon';
 const OptionButton = (props) => {
   return (
     <li className={`${classes.option} ${props.chosen && classes.chosen}`}>
+      {props.chosen && (
+        <button className={classes.delButton}>
+          <LabelDeleteIcon
+            className={classes.icon}
+            fillColor={props.color}
+            width={24}
+            height={24}
+          />
+        </button>
+      )}
       <button
+        className={classes.labelButton}
         type="button"
         style={{ backgroundColor: props.color }}
         onClick={props.onClick}
